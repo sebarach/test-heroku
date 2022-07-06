e7fb138644a9a54236ec2b936fe168212e8fa35e
Sebastian <sebastian.sc1990@gmail.com>
2022-07-06 15:08:29 -0400

    'asd'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheerio = require("cheerio");
 //const puppeteer = require('puppeteer');
 const app = express();
 const {variables,chromeOptions,chromeOptions2} = require('./utils.js');
-const { response } = require('express');
+//const { response } = require('express');
 let datos= [];
 const formatearPrecio = (precio)=> precio.replace(/[^0-9,.]+/g, "").replace(/[,.]+/g, "");
 
@@ -50,19 +50,19 @@ app.get('/',(req,res)=>{
 }
 
 
-async function scrapearRipleyPS5() {
-    try {
-        let data = await fetch(variables.urlRipleyExport);
-        let response = await data.text()
-        let $ = cheerio.load(response);
-        let precio = $('#row > div.col-xs-12.col-sm-12.col-md-5 > section.product-info > dl > div.product-price-container.product-internet-price-not-best > dt').first().text();
-        datos.push({ url:variables.urlRipleyExport, precio: precio, precioParse: formatearPrecio(precio),tienda:"Ripley" });
-       }catch (error) {
-        console.log(error);
-    }
+// async function scrapearRipleyPS5() {
+//     try {
+//         let data = await fetch(variables.urlRipleyExport);
+//         let response = await data.text()
+//         let $ = cheerio.load(response);
+//         let precio = $('#row > div.col-xs-12.col-sm-12.col-md-5 > section.product-info > dl > div.product-price-container.product-internet-price-not-best > dt').first().text();
+//         datos.push({ url:variables.urlRipleyExport, precio: precio, precioParse: formatearPrecio(precio),tienda:"Ripley" });
+//        }catch (error) {
+//         console.log(error);
+//     }
 }
 
-let allPromise = Promise.all([scrapearRipleyPS5()]);
-app.listen(4000, function() {console.log("App Corriendo en el puerto 4000");});
+// let allPromise = Promise.all([scrapearRipleyPS5()]);
+app.listen(7000, function() {console.log("App Corriendo en el puerto 4000");});
 
 
