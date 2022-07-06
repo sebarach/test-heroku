80c65e2a1d20108416b27681b1d1de9200e22006
Sebastian <sebastian.sc1990@gmail.com>
2022-07-06 14:45:41 -0400

    asd

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ const express = require('express')
 const cors = require('cors');
 const fetch = require('node-fetch');
 const cheerio = require("cheerio");
-const puppeteer = require('puppeteer');
+//const puppeteer = require('puppeteer');
 const app = express();
-const {variables,chromeOptions,chromeOptions2} = require('./utils.js')
+const {variables,chromeOptions,chromeOptions2} = require('./utils.js');
+const { response } = require('express');
 let datos= [];
 const formatearPrecio = (precio)=> precio.replace(/[^0-9,.]+/g, "").replace(/[,.]+/g, "");
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 
+// app.get('/test',(req,res)=>{
+//     console.log("Buscando Precios desde test");
+//     res.header("Access-Control-Allow-Origin", "*");
+//     allPromise.then(values => {
+//         console.log("Termino de Buscar Precios");
+//         res.json(datos);
+//       }).catch(error => {
+//           console.log(error);
+//       });
+//     });
 
-app.get('/test',(req,res)=>{
-      console.log("Buscando Precios desde test");
-      res.header("Access-Control-Allow-Origin", "*");
-      allPromise.then(values => {
-          console.log("Termino de Buscar Precios");
-          res.json(datos);
-        }).catch(error => {
-            console.log(error);
-        });
+
+app.get('/',(req,res)=>{
+        res.send("funciona");
       });
 
     async function scrapearProMovilPS5() {
