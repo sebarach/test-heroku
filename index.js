adb48679aa9a59f8a714b5a79dd6365f9f76388c
Sebastian <sebastian.sc1990@gmail.com>
2022-07-06 15:39:17 -0400

    'asd'

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ const formatearPrecio = (precio)=> precio.replace(/[^0-9,.]+/g, "").replace(/[,.
 
 app.use(cors());
 app.use(express.json());
+app.use(express.text());
 
-
-// app.get('/test',(req,res)=>{
-//     console.log("Buscando Precios desde test");
-//     res.header("Access-Control-Allow-Origin", "*");
-//     allPromise.then(values => {
-//         console.log("Termino de Buscar Precios");
-//         res.json(datos);
-//       }).catch(error => {
-//           console.log(error);
-//       });
-//     });
+app.get('/test',(req,res)=>{
+    console.log("Buscando Precios desde test");
+    res.header("Access-Control-Allow-Origin", "*");
+    allPromise.then(values => {
+        console.log("Termino de Buscar Precios");
+        res.json(datos);
+      }).catch(error => {
+          console.log(error);
+      });
+    });
 
 
 app.get('/',(req,res)=>{
@@ -50,20 +50,20 @@ app.get('/',(req,res)=>{
 }
 
 
-// async function scrapearRipleyPS5() {
-//     try {
-//         let data = await fetch(variables.urlRipleyExport);
-//         let response = await data.text()
-//         let $ = cheerio.load(response);
-//         let precio = $('#row > div.col-xs-12.col-sm-12.col-md-5 > section.product-info > dl > div.product-price-container.product-internet-price-not-best > dt').first().text();
-//         datos.push({ url:variables.urlRipleyExport, precio: precio, precioParse: formatearPrecio(precio),tienda:"Ripley" });
-//        }catch (error) {
-//         console.log(error);
-//     }
-//}
+async function scrapearRipleyPS5() {
+    try {
+        let data = await fetch(variables.urlRipleyExport);
+        let response = await data.text()
+        let $ = cheerio.load(response);
+        let precio = $('#row > div.col-xs-12.col-sm-12.col-md-5 > section.product-info > dl > div.product-price-container.product-internet-price-not-best > dt').first().text();
+        datos.push({ url:variables.urlRipleyExport, precio: precio, precioParse: formatearPrecio(precio),tienda:"Ripley" });
+       }catch (error) {
+        console.log(error);
+    }
+}
 
 const puerto = 3000;
-// let allPromise = Promise.all([scrapearRipleyPS5()]);
-app.listen(process.env.PORT, function() {console.log(`App Corriendo en el puerto ${puerto}`);});
+let allPromise = Promise.all([scrapearRipleyPS5()]);
+app.listen(process.env.PORT || 3000, function() {console.log(`App Corriendo en el puerto ${puerto}`);});
 
 
